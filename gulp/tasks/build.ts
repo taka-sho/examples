3e9930a1b46520ea5e1a3a49df66ff9c28613bfd
Keisuke Kan <9renpoto@gmail.com>
2016-04-19 18:16:32 +0900

    fix build task

--- gulp/tasks/build.ts
+++ gulp/tasks/build.ts
@@ -6,7 +6,7 @@ gulp.task('build', function (callback: Function) {
   const config: any = require('../config');
   const jade: any = require('gulp-jade')
 
-  return gulp.src(path.join(config.root, 'src/**/*.jade'))
+  return gulp.src(path.join(config.root, 'src/*/*.jade'))
     .pipe(jade({
       locales: 'en-us'
     }))
