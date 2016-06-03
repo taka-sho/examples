cfe7544b01598ff110a75ab22ae5ac6060101041
Keisuke Kan <9renpoto@gmail.com>
2016-06-03 16:46:08 +0900

    Update task

--- gulp/tasks/build_template.ts
+++ gulp/tasks/build_template.ts
@@ -2,13 +2,14 @@
 import gulp = require('gulp');
 
 gulp.task('build_template', function (callback: Function) {
-  const path: any = require('path');
-  const config: any = require('../config');
-  const jade: any = require('gulp-pug')
+  const path: any = require('path')
+  const config: any = require('../config')
+  const pug: any = require('gulp-pug')
+  const gulpIf: any = require('gulp-if')
 
-  return gulp.src(path.join(config.root, 'template/*/*.pug'))
-    .pipe(jade({
+  return gulp.src(path.join(config.root, 'template/**'))
+    .pipe(gulpIf('*.pug', pug({
       locales: 'en-us'
-    }))
+    })))
     .pipe(gulp.dest(config.dist));
 });
