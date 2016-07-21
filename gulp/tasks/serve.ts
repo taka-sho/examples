a4bc4b049d4c1d2773b6457fc4a5d639f1bff056
Keisuke Kan <kan@uncovertruth.jp>
2016-07-21 11:54:43 +0900

    chore(gulp) goodbye gulp scripts

--- gulp/tasks/serve.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='../../typings/index.d.ts' />
-import gulp = require('gulp')
-
-gulp.task('serve', () => {
-  const connect: any = require('gulp-connect')
-  connect.server({
-    root: 'dist'
-  })
-})
