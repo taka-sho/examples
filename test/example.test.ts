be77085f838aff692d813b796bcc9c0e87d98b35
taka-sho <takasho530000@gmail.com>
2016-07-26 18:44:10 +0900

    変数活用。改行・空文字修正

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,19 +1,18 @@
 /// <reference path="../typings/index.d.ts" />
 
-import * as assert from 'power-assert'
+import * as assert from 'power-assert';
 
 describe('examples test', function () {
   beforeEach(function () {
-    browser.url('https://uncovertruth.github.io/examples/t/simple.html')
+    browser.url('https://uncovertruth.github.io/examples/t/simple.html');
   })
+
   it('select a value',function(){
+    const index = 1;
+    const selector = 'select';
+    browser.selectByIndex(selector,index);
 
-    const selector = 'select'
-    const index = 1
-
-    browser
-      .selectByIndex(selector, index)
-    const val = browser.getValue('select')
-    assert.equal(val,'example2')
+    const val = browser.getValue(selector);
+    assert.equal(val,'example2');
   })
 })
