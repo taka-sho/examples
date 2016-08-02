41f17970cbb5ef143c8843a442bc7df636bb9053
heavenshell <heavenshell.jp@gmail.com>
2016-08-02 18:13:18 +0900

    Add angular-ui-router e2e test

---
+++ test/angular-ui-router.test.ts
@@ -0,0 +1,21 @@
+/// <reference path="../typings/index.d.ts" />
+
+import * as assert from 'power-assert'
+
+describe('angular-ui-router', function () {
+  const selector = 'div[ui-view] div ul li a'
+  beforeEach(function () {
+    browser.url('http://uncovertruth.github.io/examples/fw/angular-ui/ui-router/')
+  })
+
+  it('should display Top', function () {
+    const element: any = browser.element(selector)
+    assert.equal(element.getText(), 'Top')
+  })
+
+  it('should change to About', function () {
+    browser.click(selector)
+    const element: any = browser.element(selector)
+    assert.equal(element.getText(), 'About')
+  })
+})
