69b6c9e842779e1033b48548bb6d00a176f86f41
heavenshell <heavenshell.jp@gmail.com>
2016-08-02 13:58:44 +0900

    Add angular-ui-router example

---
+++ src/app/angular1/controllers/about.ts
@@ -0,0 +1,7 @@
+export default class About {
+  public static $inject = ['$scope', '$state']
+  public title: string = ''
+  constructor(protected $scope: ng.IScope, private $state: angular.ui.IStateService) {
+    this.title = 'About'
+  }
+}
---
+++ src/app/angular1/controllers/top.ts
@@ -0,0 +1,7 @@
+export default class Top {
+  public static $inject = ['$scope', '$state']
+  public title: string = ''
+  constructor(protected $scope: ng.IScope, private $state: angular.ui.IStateService) {
+    this.title = 'Top'
+  }
+}
