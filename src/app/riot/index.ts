ae934b5ece400c4d7a7888f6b23d2326fd41b744
taka-sho <takasho530000@gmail.com>
2016-11-15 16:54:01 +0900

    routeの実装(routeのrequireは未だ)

--- src/app/riot/index.ts
+++ src/app/riot/index.ts
@@ -1,3 +1,9 @@
 import * as riot from 'riot'
 import {hello} from 'tags/hello'
 riot.mount('hello')
+console.log('asd')
+
+const Route = riot.router.Route
+  riot.router.routes([
+    new Route({tag: 'hello'})
+  ])
