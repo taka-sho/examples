c2cc5517609d4972cd32eecaf704ef11d2039f5e
taka-sho <takasho530000@gmail.com>
2016-11-15 18:42:04 +0900

    tsエラーの修正

--- src/app/riot/index.ts
+++ src/app/riot/index.ts
@@ -1,11 +1,8 @@
 import * as riot from 'riot'
 import {top} from 'tags/top'
-import {about} from 'tags/about'
-riot.mount('top')
+riot.mount(top)
 
-const Route = riot.router.Route
 const DefaultRoute = riot.router.Default
 riot.router.routes([
-  new Route({tag: 'about'}),
   new DefaultRoute({tag: 'top'})
 ])
