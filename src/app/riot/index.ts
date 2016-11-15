6793e4aa3d38a818eee2126b5dcbf0fdef2ad716
taka-sho <takasho530000@gmail.com>
2016-11-15 19:11:48 +0900

    this.optでriot-routeを読み込む(tags/topがimportされてない)

--- src/app/riot/index.ts
+++ src/app/riot/index.ts
@@ -1,8 +1,8 @@
 import * as riot from 'riot'
 import {top} from 'tags/top'
-riot.mount(top)
-
+const router = this.route
 const DefaultRoute = riot.router.Default
 riot.router.routes([
   new DefaultRoute({tag: 'top'})
 ])
+riot.mount(top)
