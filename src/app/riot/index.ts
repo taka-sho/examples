82894f49b717bf0b00604e52c7b729a4cf55d6b2
taka-sho <takasho530000@gmail.com>
2016-11-16 16:58:23 +0900

    pugにてtypescriptを使用

--- src/app/riot/index.ts
+++ src/app/riot/index.ts
@@ -1,8 +1,3 @@
 import * as riot from 'riot'
-import {top} from 'tags/top'
-const router = this.route
-const DefaultRoute = riot.router.Default
-riot.router.routes([
-  new DefaultRoute({tag: 'top'})
-])
-riot.mount(top)
+
+riot.mount('top-content')
