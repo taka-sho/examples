a5114d465d5c856d884588d9619dfe905a73d453
taka-sho <takasho530000@gmail.com>
2016-11-29 18:27:36 +0900

    lint直し

--- src/app/riot/index.ts
+++ src/app/riot/index.ts
@@ -1,4 +1,4 @@
-import * as riot from 'riot';
-import './tags/top';
-import './tags/about';
-riot.mount('*');
+import * as riot from 'riot'
+import './tags/top'
+import './tags/about'
+riot.mount('*')
