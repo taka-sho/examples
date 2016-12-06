9eaa37c17013b363c7a39a30550ebbd73a779637
taka-sho <takasho530000@gmail.com>
2016-12-06 17:10:11 +0900

    indexにroutingを記載

--- src/app/riot/index.ts
+++ src/app/riot/index.ts
@@ -1,4 +1,13 @@
 import '../../../tags/top'
 import '../../../tags/about'
+const route = require('riot-route')
 
 riot.mount('*')
+const routeTag = 'top'
+route('/', function() {
+  riot.mount(routeTag, 'top')
+})
+route('/about', function() {
+  riot.mount(routeTag, 'about')
+})
+route.start(true)
