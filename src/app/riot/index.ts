1c59166a14eae4facb9dc5ffa31fe5688e472fa9
taka-sho <takasho530000@gmail.com>
2016-12-07 17:42:42 +0900

    changeVirtualrUrlの実装

--- src/app/riot/index.ts
+++ src/app/riot/index.ts
@@ -1,13 +1,24 @@
 import '../../../tags/top'
 import '../../../tags/about'
 import * as riot from 'riot'
+import Tracker from '../ud'
 const route = require('riot-route')
 
-const routeTag = 'top'
-route('/', function() {
-  riot.mount(routeTag, 'top')
-})
-route('/about', function() {
-  riot.mount(routeTag, 'about')
-})
-route.start(true)
+namespace riotRoute {
+  const routeTag = 'top'
+  const tracker = new Tracker()
+
+  function setTag (setTagName: string, tagFileName: string) {
+    riot.mount(setTagName, tagFileName)
+    tracker.changeVirtualUrl()
+  }
+
+  route('/', function() {
+    setTag(routeTag, 'top')
+  })
+  route('/about', function() {
+    setTag(routeTag, 'about')
+  })
+
+  route.start(true)
+}
