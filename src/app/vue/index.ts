dff97bc10d405a693edaac9a1d4ec830ec2ebef2
taka-sho <takasho530000@gmail.com>
2016-12-14 00:44:01 +0900

    vue-routerno

--- src/app/vue/index.ts
+++ src/app/vue/index.ts
@@ -1,10 +1,26 @@
 import * as Vue from 'vue'
-// import * as VueRouter from 'vue-router'
-// import Tracker from '../ud'
+import * as VueRouter from 'vue-router'
+import Tracker from '../ud'
+const tracker = new Tracker()
 
-new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello'
-  }
+Vue.use(VueRouter)
+
+const top = { template: '<a href="#/about">about</a>' }
+const about = { template: '<a href="#/">top</a>' }
+
+const routes = [
+  {path: '/', component: top},
+  {path: '/about', component: about}
+]
+
+const router = new VueRouter({
+  routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
+
+router.afterEach(function () {
+  tracker.changeVirtualUrl()
 })
