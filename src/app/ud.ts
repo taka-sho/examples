3f6ea80ba2f8714f319bffd1610ae83c177e504e
taka-sho <takasho530000@gmail.com>
2016-12-14 19:24:59 +0900

    url検知の変更

--- src/app/ud.ts
+++ src/app/ud.ts
@@ -5,8 +5,9 @@ declare var ud: USERDIVETracker.USERDIVEObject
 export default class Tracker {
   getUrl (): string {
     const origin: string = 'http://uncovertruth.github.io'
-    if (/#\/about/.test(location.href)) {
+    if (/#\/about/.test(location.href) || /\/#\/about/.test(location.href)) {
       return `${origin}/examples/t/virtualurl.html?about=1`
+
     }
     return `${origin}/examples/t/virtualurl.html?default=1`
   }
