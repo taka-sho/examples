0b51633cbe2ab1356f9be484e7b1f9714e28a336
Keisuke Kan <kan@uncovertruth.jp>
2016-12-26 19:44:57 +0900

    chore(package) remove typings

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,5 +1,3 @@
-/// <reference path="../typings/index.d.ts" />
-
 import * as assert from 'power-assert'
 
 describe('examples test', function () {
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,11 @@ module.exports = {
   plugins: [
     new webpack.ProvidePlugin({
       riot: 'riot'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
     })
   ]
 }
