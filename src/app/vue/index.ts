1967ce4f5533e87a89ee676996d737aa541efa42
taka-sho <takasho530000@gmail.com>
2016-12-27 16:36:53 +0900

    vue-routerを動作させる

--- src/app/vue/index.ts
+++ src/app/vue/index.ts
@@ -2,28 +2,14 @@ import * as Vue from 'vue'
 import * as VueRouter from 'vue-router'
 import Tracker from '../ud'
 const tracker = new Tracker()
-<<<<<<< HEAD
-
 Vue.use(VueRouter)
 
-const top = { template: '<a href="#/about">about</a>' }
-const about = { template: '<a href="#/">top</a>' }
-
-const routes = [
-  {path: '/', component: top},
-  {path: '/about', component: about}
-=======
-tracker.changeVirtualUrl()
-
-Vue.use(VueRouter)
-
-const top = { template: '<div>これはtop</div>' }
-const about = { template: '<div>これはabout</div>' }
+const top = { template: '<router-link to=\'about\'>about</router-link>' }
+const about = { template: '<router-link to=\'/\'>top</router-link>' }
 
 const routes = [
   {path: '/', name: 'top', component: top},
   {path: '/about', name: 'about', component: about}
->>>>>>> 56dbacb81204f6d4f489f961933124372309d70a
 ]
 
 const router = new VueRouter({
