452656e166a6f45c5976c64de240b090061d94af
Keisuke Kan <kan@uncovertruth.co.jp>
2017-02-10 15:28:40 +0900

    fix(test) update timeout

--- test/react-router.test.ts
+++ test/react-router.test.ts
@@ -5,13 +5,13 @@ const getUrl = `return UDTracker.Config.getOverrideUrl();`
 describe('react-router', function () {
   it('should display Top', () => {
     browser.url('http://localhost:8080/fw/reactjs/react-router')
-    browser.pause(3000)
+    browser.pause(5000)
     assert.equal(browser.execute(getUrl).value, 'http://uncovertruth.github.io/examples/t/virtualurl.html?default=1')
   })
 
   it('should change to About', () => {
     browser.click('a#about')
-    browser.pause(3000)
+    browser.pause(5000)
     assert.equal(browser.execute(getUrl).value, 'http://uncovertruth.github.io/examples/t/virtualurl.html?about=1')
   })
 })
