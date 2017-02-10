15a591f4609fc29a0256bd42affbbcb48bdd7eca
Keisuke Kan <kan@uncovertruth.co.jp>
2017-02-10 14:44:59 +0900

    fix(test) fix elements

--- test/react-router.test.ts
+++ test/react-router.test.ts
@@ -1,7 +1,8 @@
 import * as assert from 'assert'
 
 const getUrl = `return UDTracker.Config.getOverrideUrl();`
-describe.only('react-router', function () {
+
+describe('react-router', function () {
   it('should display Top', () => {
     browser.url('http://localhost:8080/fw/reactjs/react-router')
     browser.pause(3000)
@@ -9,9 +10,8 @@ describe.only('react-router', function () {
   })
 
   it('should change to About', () => {
-    const selector = 'div div ul li a'
-    browser.click(selector)
+    browser.click('a#about')
     browser.pause(3000)
-    assert.equal(browser.execute(getUrl).value, 'http://uncovertruth.github.io/examples/t/virtualurl.html?default=1')
+    assert.equal(browser.execute(getUrl).value, 'http://uncovertruth.github.io/examples/t/virtualurl.html?about=1')
   })
 })
