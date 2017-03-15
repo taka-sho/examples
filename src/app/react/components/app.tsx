83e6bfa84efb7d1bacd291760cb7823759ad70ef
taka-sho <takasho530000@gmail.com>
2017-03-15 17:43:13 +0900

    chore(react/comps/app) refactor a space

--- src/app/react/components/app.tsx
+++ src/app/react/components/app.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 
 export class App extends React.Component<any, any> {
-  render() {
+  render () {
     return (
       <div>
         <ul>
