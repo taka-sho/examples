33e57f2d1c865a65c8daadabede38c32cbc4a2b8
taka-sho <takasho530000@gmail.com>
2017-06-07 16:30:42 +0900

    chore(vue) rename routes

--- src/app/vue/index.ts
+++ src/app/vue/index.ts
@@ -4,12 +4,13 @@ import Userdive from 'userdive'
 const _ud = Userdive()
 Vue.use(VueRouter)
 
-const top = { template: '<router-link to="about">about</router-link>' }
-const about = { template: '<router-link to="/">top</router-link>' }
+const sample1 = { template: '<router-link to="sample2">Sample2</router-link>' }
+const sample2 = { template: '<router-link to="sample1">Sample1</router-link>' }
 
 const routes = [
-  {path: '/', name: 'top', component: top},
-  {path: '/about', name: 'about', component: about}
+  {path: '/sample1', name: 'sample1', component: sample1},
+  {path: '/sample2', name: 'sample2', component: sample2},
+  {path: '/**', redirect: 'sample1'}
 ]
 
 const router = new VueRouter({
