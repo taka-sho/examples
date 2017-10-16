fac220b7893fbf3c7fcb0f8b052775ef8c5146db
Keisuke Kan <9renpoto@gmail.com>
2017-10-16 19:25:36 +0900

    chore(package) update dependencies (#97)

--- src/app/vue/index.ts
+++ src/app/vue/index.ts
@@ -1,5 +1,5 @@
-import * as Vue from 'vue'
-import * as VueRouter from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import userdive from 'userdive'
 const _ud = userdive()
 Vue.use(VueRouter)
