e9cb7e948ff36ecf9508e2fb8fc6a257947e86dd
Shoota Kumano <shoota.dev@gmail.com>
2017-10-31 14:16:17 +0900

    chore(package): update @uncovertruth/tslint-config to version 1.2.0 (#361)
    * chore(package): update @uncovertruth/tslint-config to version 1.2.0
    
    * fix lint

--- test/example.test.ts
+++ test/example.test.ts
@@ -11,21 +11,21 @@ describe('examples test', function () {
     const email: string = 'userdive@example.com'
     const el: any = element(by.css('.form-group:first-of-type .form-controll'))
 
-    for (const str of email.split('') ) {
+    for (const str of email.split('')) {
       el.sendKeys(str)
     }
-    el.getAttribute('value').then((val) => {
+    el.getAttribute('value').then(val => {
       assert.equal(val, email)
       done()
     })
   })
 
   it('select a value', function () {
-    element.all(by.tagName('select option')).then((items) => {
+    element.all(by.tagName('select option')).then(items => {
       items[1].click()
       const dropDown: any = element(by.css('select'))
-      dropDown.getAttribute('value').then((val) => {
-        assert.equal(val,'example2')
+      dropDown.getAttribute('value').then(val => {
+        assert.equal(val, 'example2')
       })
     })
   })
